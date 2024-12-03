ba58a3b8b9876a6e671689aa70cee0e98ae4613a
Kauã Marques Rodrigues <kauamarquesrodrigues423@gmail.com>
2024-12-03 00:01:41 -0300

    Delete api directory

--- api/src/main/java/com/ecommerce/api/dto/CartaoDto.java
+++
@@ -1,4 +0,0 @@
-package com.ecommerce.api.dto;
-
-public record CartaoDto(String nome_cart, int numero_cart, double val_cart, int cvv_cart) {
-}
--- api/src/main/java/com/ecommerce/api/dto/ProdutosDto.java
+++
@@ -1,5 +0,0 @@
-package com.ecommerce.api.dto;
-
-public record ProdutosDto(String nome, String descricao,
-                          String preco, String foto) {
-}
